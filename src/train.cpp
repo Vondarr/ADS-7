f9d16507be88e867a2714d831f79f56ee50916d8
Vondarr <vladislavradnov6@gmail.com>
2026-05-28 15:15:13 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,52 +1,52 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
 
-Train::Train() : head(nullptr), totalOps(0) {}
+Train::Train() : first(nullptr), countOp(0) {}
 
-int Train::getOpCount() { 
-    return totalOps; 
+int Train::getOpCount() {
+  return countOp;
 }
 
-void Train::addCar(bool light) { 
-    Car *freshCar = new Car;
-    freshCar->light = light;
-    
-    if (head == nullptr) {
-        head = freshCar;
-        freshCar->next = freshCar;
-        freshCar->prev = freshCar;
-        return;
-    }
-    
-    Car *tail = head->prev;
-    freshCar->next = head;
-    freshCar->prev = tail;
-    tail->next = freshCar;
-    head->prev = freshCar;
+void Train::addCar(bool light) {
+  Car *freshCar = new Car;
+  freshCar->light = light;
+
+  if (first == nullptr) {
+    first = freshCar;
+    freshCar->next = freshCar;
+    freshCar->prev = freshCar;
+    return;
+  }
+
+  Car *tail = first->prev;
+  freshCar->next = first;
+  freshCar->prev = tail;
+  tail->next = freshCar;
+  first->prev = freshCar;
 }
 
-int Train::getLength() { 
-    Car *current = head;
-    current->light = true;
-    
-    while (true) {
-        int distance = 0;
-        
-        do {
-            current = current->next;
-            totalOps++;
-            distance++;
-        } while (!current->light);
-        
-        current->light = false;
-        
-        for (int i = 0; i < distance; i++) {
-            current = current->prev;
-            totalOps++;
-        }
-        
-        if (!current->light) {
-            return distance;
-        }
+int Train::getLength() {
+  Car *current = first;
+  current->light = true;
+
+  while (true) {
+    int distance = 0;
+
+    do {
+      current = current->next;
+      countOp++;
+      distance++;
+    } while (!current->light);
+
+    current->light = false;
+
+    for (int i = 0; i < distance; i++) {
+      current = current->prev;
+      countOp++;
     }
+
+    if (!current->light) {
+      return distance;
+    }
+  }
 }
